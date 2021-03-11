7cb5101a3512d190a220ba7d3a7d49c144bf8b1b
Sim-ai <71258346+Sim-ai@users.noreply.github.com>
2021-03-11 15:19:54 +0800

    Update class_names.py

--- mmdet/core/evaluation/class_names.py
+++ mmdet/core/evaluation/class_names.py
@@ -6,10 +6,14 @@ def wider_face_classes():
 
 
 def voc_classes():
-    return [
-        'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat',
-        'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person',
-        'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
+#     return [
+#         'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat',
+#         'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person',
+#         'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
+#     ]    commented by xp
+
+return [
+        '气孔', '夹渣', '焊瘤', '裂纹', '咬边', '未熔合', '未焊透', '烧穿', '焊缝', '成形不良', '内凹', '异物', '伪缺陷'
     ]
 
 
