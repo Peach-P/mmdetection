3419070840bc3ce0828bc8ff78549d2989175be6
Sim-ai <71258346+Sim-ai@users.noreply.github.com>
2021-03-13 10:55:49 +0800

    Update voc.py

--- mmdet/datasets/voc.py
+++ mmdet/datasets/voc.py
@@ -10,16 +10,16 @@ from .xml_style import XMLDataset
 @DATASETS.register_module()
 class VOCDataset(XMLDataset):
     
-    CLASSES =   ('飞机', '自行车', '鸟', '船', '瓶子', '汽车', '小汽车', '猫',
-        '椅子', '牛', '犀牛', '狗', '马', '摩托', '人',
-        '盆栽植物', '绵羊', '沙发', '火车', '电视监视器') 
+#     CLASSES =   ('飞机', '自行车', '鸟', '船', '瓶子', '汽车', '小汽车', '猫',
+#         '椅子', '牛', '犀牛', '狗', '马', '摩托', '人',
+#         '盆栽植物', '绵羊', '沙发', '火车', '电视监视器') 
 
 #     CLASSES = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car',
 #                'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
 #                'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train',
 #                'tvmonitor')       
             
-#     CLASSES = ('气孔', '夹渣', '焊瘤', '裂纹', '咬边', '未熔合', '未焊透', '烧穿', '焊缝', '成形不良', '内凹', '异物', '伪缺陷')
+    CLASSES = ('气孔', '夹渣', '焊瘤', '裂纹', '咬边', '未熔合', '未焊透', '烧穿', '焊缝', '成形不良', '内凹', '异物', '伪缺陷')
 
     def __init__(self, **kwargs):
         super(VOCDataset, self).__init__(**kwargs)
