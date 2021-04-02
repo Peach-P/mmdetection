88009acc5bc7f5b196015ce2d15190bd4dd7ebab
Sim-ai <71258346+Sim-ai@users.noreply.github.com>
2021-04-02 20:10:44 +0800

    Update class_names.py

--- mmdet/core/evaluation/class_names.py
+++ mmdet/core/evaluation/class_names.py
@@ -15,7 +15,7 @@ def voc_classes():
 #         '气孔', '夹渣', '焊瘤', '裂纹', '咬边', '未熔合', '未焊透', '烧穿', '焊缝', '成形不良', '内凹', '异物', '伪缺陷'
 #     ]   
     return [
-            'porosity', 'lack_of_penetration', 'lack_of_fusion', 'overlap', '', 'foreign_object', 'weld'
+            'porosity', 'lack_of_penetration', 'lack_of_fusion', 'overlap', 'foreign_object', 'weld'
         ]   
 #     return [
 #         'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat',
