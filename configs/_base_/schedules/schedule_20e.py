787937b0e755f50467edeb2a56d249c226097163
Sim-ai <71258346+Sim-ai@users.noreply.github.com>
2021-04-16 19:28:06 +0800

    Update schedule_20e.py

--- configs/_base_/schedules/schedule_20e.py
+++ configs/_base_/schedules/schedule_20e.py
@@ -1,5 +1,6 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+#optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001) commented by xp
+optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
@@ -9,4 +10,4 @@ lr_config = dict(
     warmup_ratio=0.001,
     step=[16, 19])
 # runner = dict(type='EpochBasedRunner', max_epochs=20) #commented by xp
-runner = dict(type='EpochBasedRunner', max_epochs=200)
+runner = dict(type='EpochBasedRunner', max_epochs=500)
