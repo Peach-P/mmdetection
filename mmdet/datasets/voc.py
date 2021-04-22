ec49a6f296e5bcf0a0713e4bab9e3b7c087e91c2
Sim-ai <71258346+Sim-ai@users.noreply.github.com>
2021-04-22 08:48:16 +0800

    Update voc.py

--- mmdet/datasets/voc.py
+++ mmdet/datasets/voc.py
@@ -16,7 +16,8 @@ class VOCDataset(XMLDataset):
 #                'tvmonitor')       
             
 #     CLASSES = ('气孔', '夹渣', '焊瘤', '裂纹', '咬边', '未熔合', '未焊透', '烧穿', '焊缝', '成形不良', '内凹', '异物', '伪缺陷')
-    CLASSES = ('焊缝', '未焊透', '未熔合', '气孔')
+#     CLASSES = ('焊缝', '未焊透', '未熔合', '气孔')
+      CLASSES = ('未焊透', '未熔合', '气孔')
 
     def __init__(self, **kwargs):
         super(VOCDataset, self).__init__(**kwargs)
