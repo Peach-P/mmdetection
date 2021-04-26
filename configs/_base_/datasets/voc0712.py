eca897de1ce3e22e1f40be0448be1b023807692a
Sim-ai <71258346+Sim-ai@users.noreply.github.com>
2021-04-26 09:32:56 +0800

    Update voc0712.py

--- configs/_base_/datasets/voc0712.py
+++ configs/_base_/datasets/voc0712.py
@@ -5,11 +5,13 @@ data_root = '/xiaopeng/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
+#     dict(type='LoadImageFromFile'),  # commented by xp
+    dict(type='LoadImageFromFile', to_float32=True), # add by xp
     dict(type='LoadAnnotations', with_bbox=True),
-#     dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
-    dict(type='Resize', img_scale=(300, 400), keep_ratio=True),
+#     dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),  
+    dict(type='Resize', img_scale=(300, 350), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='PhotoMetricDistortion'),   # add by xp
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
@@ -20,7 +22,7 @@ test_pipeline = [
     dict(
         type='MultiScaleFlipAug',
 #         img_scale=(1000, 600),
-        img_scale=(300, 400),
+        img_scale=(300, 350),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -32,7 +34,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
+#     samples_per_gpu=2, # commented by xp
+    samples_per_gpu=8,
     workers_per_gpu=2,
     train=dict(
         type='RepeatDataset',
