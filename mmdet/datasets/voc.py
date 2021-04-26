c31b1a4bdb3f06ef32a4f56c1371be730c520460
Sim-ai <71258346+Sim-ai@users.noreply.github.com>
2021-04-26 11:02:03 +0800

    Update voc.py

--- mmdet/datasets/voc.py
+++ mmdet/datasets/voc.py
@@ -10,7 +10,7 @@ from .xml_style import XMLDataset
 @DATASETS.register_module()
 class VOCDataset(XMLDataset):
      
-    CLASSES = ('气孔', '夹渣', '焊瘤', '裂纹', '咬边', '未熔合', '未焊透', '烧穿',  '成形不良', '内凹',  '伪缺陷')
+    CLASSES = ('气孔', '夹渣', '焊瘤', '裂纹', '咬边', '未熔合', '未焊透', '烧穿', '焊缝', '成形不良', '内凹', '异物', '伪缺陷')
     
 #     CLASSES = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car',
 #                'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
