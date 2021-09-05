f08b80f9ea728482986e69990c186762573070da
Sim-ai <71258346+Sim-ai@users.noreply.github.com>
2021-09-05 15:45:56 +0800

    Update predict_hf.py

--- tools/predict_hf.py
+++ tools/predict_hf.py
@@ -17,7 +17,7 @@ def Crop_hf(root_dir, checkpoint_file_hanfeng, config_file_hangfeng, crop_flag=T
         imgCrop_all = []
         result_hanfeng = inference_detector(model_hanfeng, image_path)
         print('result_hf',result_hanfeng)
-        if result_hanfeng[0].size == 0: ## 需要根据个人的修改*************************
+        if result_hanfeng[8].size == 0: ## 需要根据个人的修改*************************
             print("未检测到焊缝！")
         else:
             print("焊缝存在！")
@@ -26,11 +26,11 @@ def Crop_hf(root_dir, checkpoint_file_hanfeng, config_file_hangfeng, crop_flag=T
             if not isExists:
                 os.makedirs(tmp_path) 
                 print(tmp_path+' 创建成功')
-            print('hf_geshu',result_hanfeng[0].shape[0])
-            for j in range(result_hanfeng[0].shape[0]):
+            print('hf_geshu',result_hanfeng[8].shape[0])
+            for j in range(result_hanfeng[8].shape[0]):
               print('j_wei', j)
-            for j in range(result_hanfeng[0].shape[0]):
-              bbox_ = [int(i) for i in result_hanfeng[0][j][0:-1]]# 获得焊缝的4个坐标
+            for j in range(result_hanfeng[8].shape[0]):
+              bbox_ = [int(i) for i in result_hanfeng[8][j][0:-1]]# 获得焊缝的4个坐标
               print('bbox_ coordinat', bbox_)
               if crop_flag:
                 imgCrop_all.append(bbox_) 
@@ -39,19 +39,14 @@ def Crop_hf(root_dir, checkpoint_file_hanfeng, config_file_hangfeng, crop_flag=T
                   image = cv2.imread(image_path, -1)
                   new_img = image[img[1]:img[3], img[0]:img[2]]
                   new_img = Image.fromarray(np.uint16(new_img))
-                  name = image_path.split('\\')[-1][:-4]
+                  name = image_path.split('/')[-1][:-4]
                   save_path = os.path.join(root_dir, "tmp/"+name+'_'+str(i)+'.tif')
                   new_img.save(save_path)
 
 
 if __name__ == '__main__':
-    root_dir = r"D:\PHD_research\Model_train\object_detection\mmdetection_crop_hf\mmdetection-2.7.0\test_crop_hf" #存放要检测的图片
+    root_dir = '/xiaopeng/Have_done_Dataset/Binary_classification/random_data/16bit_train/good/' #存放要检测的图片
     crop_flag = True
-    res_path = os.path.join(root_dir,"res")
-    isExists=os.path.exists(res_path)
-    if not isExists:
-        os.makedirs(res_path) 
-        print(res_path+' 创建成功')
-    checkpoint_file_hanfeng = "D:/PHD_research/Model_train/object_detection/mmdetection_crop_hf/mmdetection-2.7.0/hf_cfg_dg/epoch_12.pth"
-    config_file_hangfeng = 'D:/PHD_research/Model_train/object_detection/mmdetection_crop_hf/mmdetection-2.7.0/hf_cfg_dg/cascade_rcnn_hrnetv2p_w32_20e_coco.py'
+    checkpoint_file_hanfeng = '/xiaopeng/backup/object_detection/hf/epoch_5.pth'
+    config_file_hangfeng = '/xiaopeng/backup/object_detection/hf/cascade_rcnn_hrnetv2p_w32_20e_coco.py'
     Crop_hf(root_dir, checkpoint_file_hanfeng, config_file_hangfeng, crop_flag)
