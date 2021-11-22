7d147b9e651e2ce3dd20a408d2351b670856141a
Peach-P <93329174+Peach-P@users.noreply.github.com>
2021-11-22 10:35:31 +0800

    Update voc0712.py

--- configs/_base_/datasets/voc0712.py
+++ configs/_base_/datasets/voc0712.py
@@ -1,7 +1,7 @@
 # dataset settings
 dataset_type = 'VOCDataset'
 #data_root = 'data/VOCdevkit/'
-data_root = '/xiaopeng/cascade-13class-dataset/'
+data_root = '/cui/dg_dataset/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -9,7 +9,7 @@ train_pipeline = [
     dict(type='LoadImageFromFile', to_float32=True), # add by xp
     dict(type='LoadAnnotations', with_bbox=True),
 #     dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),  
-    dict(type='Resize', img_scale=(300, 350), keep_ratio=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='PhotoMetricDistortion'),   # add by xp
     dict(type='Normalize', **img_norm_cfg),
@@ -22,7 +22,7 @@ test_pipeline = [
     dict(
         type='MultiScaleFlipAug',
 #         img_scale=(1000, 600),
-        img_scale=(300, 350),
+        img_scale=(1333, 800),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
